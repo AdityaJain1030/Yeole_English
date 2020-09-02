2f5654ee7adc007886739ca6f0bc3c06b200728d
AdityaJain1030 <50159919+AdityaJain1030@users.noreply.github.com>
2020-09-01 20:27:32 -0700

    someone plz fix me typescript

--- index.js
+++ index.js
@@ -1,23 +1,4 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -27,55 +8,134 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
 };
-Object.defineProperty(exports, "__esModule", { value: true });
-const Discord = __importStar(require("discord.js"));
-const cleverbot_free_1 = __importDefault(require("cleverbot-free"));
-const puppeteer_1 = __importDefault(require("puppeteer"));
-const dotenv_1 = require("dotenv");
-const secrets = dotenv_1.config();
-const client = new Discord.Client();
+exports.__esModule = true;
+var Discord = require("discord.js");
+var cleverbot = require("cleverbot-free");
+var puppeteer = require("puppeteer");
+var dotenv_1 = require("dotenv");
+var secrets = dotenv_1.config();
+var client = new Discord.Client();
 client.login(process.env.TOKEN);
-let browser, _page;
+var browser, _page;
 function sleep(milliseconds) {
-    const date = Date.now();
-    let currentDate = null;
+    var date = Date.now();
+    var currentDate = null;
     do {
         currentDate = Date.now();
     } while (currentDate - date < milliseconds);
 }
-const endBrowser = (_browser) => __awaiter(void 0, void 0, void 0, function* () {
-    yield _browser.close();
-});
-const startBrowser = () => __awaiter(void 0, void 0, void 0, function* () {
-    const b = yield puppeteer_1.default.launch();
-    const e2s = yield b.newPage();
-    yield e2s.goto('https://lingojam.com/ShakespeareanEnglishtoModernEnglish');
-    return [b, e2s];
-});
-const translate = (page, text) => __awaiter(void 0, void 0, void 0, function* () {
-    const input = yield page.$('textarea[id=english-text]');
-    yield input.click({ clickCount: 3 });
-    yield input.type(text);
-    sleep(400);
-    const e = yield page.$('textarea[id=ghetto-text]');
-    const r = yield (yield e.getProperty('value')).jsonValue();
-    return r;
-});
-client.on('ready', () => __awaiter(void 0, void 0, void 0, function* () {
-    let x = yield startBrowser();
-    browser = x[0];
-    _page = x[1];
-    console.log('ready');
-}));
-client.on('message', (msg) => __awaiter(void 0, void 0, void 0, function* () {
-    if (msg.content.startsWith(process.env.PREFIX) && msg.author.bot === false) {
-        msg.channel.startTyping();
-        const x = yield cleverbot_free_1.default(msg.content.slice(1, msg.content.length));
-        const old = yield translate(_page, x);
-        msg.channel.send(`\`${old}\``);
-        msg.channel.stopTyping();
-    }
-}));
+var endBrowser = function (_browser) { return __awaiter(void 0, void 0, void 0, function () {
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0: return [4 /*yield*/, _browser.close()];
+            case 1:
+                _a.sent();
+                return [2 /*return*/];
+        }
+    });
+}); };
+var startBrowser = function () { return __awaiter(void 0, void 0, void 0, function () {
+    var b, e2s;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0: return [4 /*yield*/, puppeteer.launch({ args: [
+                        '--no-sandbox',
+                        '--disable-setuid-sandbox',
+                    ] })];
+            case 1:
+                b = _a.sent();
+                return [4 /*yield*/, b.newPage()];
+            case 2:
+                e2s = _a.sent();
+                return [4 /*yield*/, e2s.goto('https://lingojam.com/ShakespeareanEnglishtoModernEnglish')];
+            case 3:
+                _a.sent();
+                return [2 /*return*/, [b, e2s]];
+        }
+    });
+}); };
+var translate = function (page, text) { return __awaiter(void 0, void 0, void 0, function () {
+    var input, e, r;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0: return [4 /*yield*/, page.$('textarea[id=english-text]')];
+            case 1:
+                input = _a.sent();
+                return [4 /*yield*/, input.click({ clickCount: 3 })];
+            case 2:
+                _a.sent();
+                return [4 /*yield*/, input.type(text)];
+            case 3:
+                _a.sent();
+                sleep(400);
+                return [4 /*yield*/, page.$('textarea[id=ghetto-text]')];
+            case 4:
+                e = _a.sent();
+                return [4 /*yield*/, e.getProperty('value')];
+            case 5: return [4 /*yield*/, (_a.sent()).jsonValue()];
+            case 6:
+                r = _a.sent();
+                return [2 /*return*/, r];
+        }
+    });
+}); };
+client.on('ready', function () { return __awaiter(void 0, void 0, void 0, function () {
+    var x;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0: return [4 /*yield*/, startBrowser()];
+            case 1:
+                x = _a.sent();
+                browser = x[0];
+                _page = x[1];
+                console.log('ready');
+                return [2 /*return*/];
+        }
+    });
+}); });
+client.on('message', function (msg) { return __awaiter(void 0, void 0, void 0, function () {
+    var x, old;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                if (!(msg.content.startsWith(process.env.PREFIX) && msg.author.bot === false)) return [3 /*break*/, 3];
+                msg.channel.startTyping();
+                return [4 /*yield*/, cleverbot(msg.content.slice(1, msg.content.length))];
+            case 1:
+                x = _a.sent();
+                return [4 /*yield*/, translate(_page, x)];
+            case 2:
+                old = _a.sent();
+                msg.channel.send("`" + old + "`");
+                _a.label = 3;
+            case 3: return [2 /*return*/];
+        }
+    });
+}); });
--- index.ts
+++ index.ts
@@ -1,12 +1,12 @@
 import * as Discord from 'discord.js'
-import cleverbot from 'cleverbot-free' 
-import puppeteer from 'puppeteer'
+import * as cleverbot from 'cleverbot-free' 
+import * as puppeteer from 'puppeteer'
 
 import {config} from 'dotenv'
 const secrets = config()
 const client = new Discord.Client()
 client.login(process.env.TOKEN)
-let browser, _page
+let browser, _page: puppeteer.Browser | puppeteer.Page
 
 function sleep(milliseconds: number) {
     const date = Date.now();
@@ -16,16 +16,19 @@ function sleep(milliseconds: number) {
     } while (currentDate - date < milliseconds);
   }
 
-const endBrowser = async (_browser) => {
+const endBrowser = async (_browser: { close: () => any }) => {
     await _browser.close();
 }
 const startBrowser = async() => {
-    const b = await puppeteer.launch()
+    const b = await puppeteer.launch( {args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+      ]})
     const e2s = await b.newPage()
     await e2s.goto('https://lingojam.com/ShakespeareanEnglishtoModernEnglish')
     return [b, e2s]
 }
-const translate = async(page, text: string) => {
+const translate = async(page: { $: (arg0: string) => any }, text: string) => {
     const input = await page.$('textarea[id=english-text]');
     await input.click({ clickCount: 3 })
     await input.type(text);
@@ -41,11 +44,10 @@ client.on('ready', async() => {
     console.log('ready')
 })
 client.on('message', async (msg) => {
-    if(msg.content.startsWith(process.env.PREFIX) && msg.author.bot===false){
+    if(msg.content.startsWith((process.env.PREFIX as string)) && msg.author.bot===false){
         msg.channel.startTyping()
         const x = await cleverbot(msg.content.slice(1, msg.content.length))
         const old = await translate(_page, x)
         msg.channel.send(`\`${old}\``)
-        msg.channel.stopTyping()
     }
 })
